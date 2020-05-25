0eb2ec5f6230852d7843a78c59d25a8098f297c4
teoreggae <teoreggae5454@gmail.com>
2020-05-25 18:52:04 +0300

    Updates

---
+++ precache-manifest.64c3d5c352700315ea8f66f46e6e9c5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcad9aa6f447b13d40a4526f869cfb54",
+    "url": "/randomquotemachine/index.html"
+  },
+  {
+    "revision": "8aa794513cc841950c7b",
+    "url": "/randomquotemachine/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc5a64d3cf8a5416eadc",
+    "url": "/randomquotemachine/static/js/2.90cd9534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomquotemachine/static/js/2.90cd9534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa794513cc841950c7b",
+    "url": "/randomquotemachine/static/js/main.86db40c1.chunk.js"
+  },
+  {
+    "revision": "5a58648c8c3c0c9a1938",
+    "url": "/randomquotemachine/static/js/runtime-main.f8373e3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/randomquotemachine/static/media/logo.5d5d9eef.svg"
+  }
+]);
